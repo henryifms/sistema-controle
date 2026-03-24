43f78432d0366f983243d79ca8a2d9ab0f26e6cd
henrythcampos@gmail.com <henry.campos@estudante.ifms.edu.br>
2026-03-24 11:17:11 -0300

    feat(main): Enviando funcionalidades de hoje

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,79 +1,44 @@
-#include <SPI.h>
-#include <UIPEthernet.h>
+const int btn1 = 24;
+const int btn2 = 26;
 
-byte mac[] = {0x02,0x13,0x37,0xAA,0xBB,0xCC};
+bool travadoTeclado = false;
+bool travadoTela = false;
 
-IPAddress ip(10,8,34,177);
-IPAddress gateway(10,8,32,1);
-IPAddress subnet(255,255,248,0);
-IPAddress dnsServer(10,8,32,1);
-
-IPAddress server(10,8,34,16); // PC
-
-const int led = 22;
-const int botao1 = 24;
-const int botao2 = 26;
-
-EthernetClient client;
+unsigned long lastPress1 = 0;
+unsigned long lastPress2 = 0;
+const int debounceDelay = 250;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(botao1, INPUT_PULLUP);
-  pinMode(botao2, INPUT_PULLUP);
-  pinMode(led, OUTPUT);
-
-  Serial.println("Inicializando rede...");
-
-  Ethernet.begin(mac, ip, dnsServer, gateway, subnet);
-
-  delay(1000);
-
-  Serial.print("IP: ");
-  Serial.println(Ethernet.localIP());
-
-  digitalWrite(led, HIGH);
-}
-
-void enviarComando(const char* comando) {
-  Serial.print("Enviando: ");
-  Serial.println(comando);
-
-  if (client.connect(server, 8000)) {
-    Serial.println("CONECTOU!");
-
-    // HTTP SIMPLES (mais compatível)
-    client.print("GET /");
-    client.print(comando);
-    client.println(" HTTP/1.0");
-
-    client.println("Host: 10.8.34.16");
-    client.println();
-
-    delay(100);
-
-    // Debug da resposta
-    while (client.available()) {
-      char c = client.read();
-      Serial.write(c);
-    }
-
-    client.stop();
-    Serial.println("\nComando enviado!");
-  } else {
-    Serial.println("FALHA AO CONECTAR");
-  }
+  pinMode(btn1, INPUT_PULLUP);
+  pinMode(btn2, INPUT_PULLUP);
 }
 
 void loop() {
+  unsigned long agora = millis();
 
-  if (digitalRead(botao1) == LOW) {
-    enviarComando("bloquear");
-    delay(300);
+  if (digitalRead(btn1) == LOW && (agora - lastPress1 > debounceDelay)) {
+    travadoTeclado = !travadoTeclado;
+
+    if (travadoTeclado) {
+      Serial.println("BLOQUEAR_TECLADO");
+    } else {
+      Serial.println("DESBLOQUEAR_TECLADO");
+    }
+
+    lastPress1 = agora;
   }
 
-  if (digitalRead(botao2) == LOW) {
-    enviarComando("desbloquear");
-    delay(300);
+  if (digitalRead(btn2) == LOW && (agora - lastPress2 > debounceDelay)) {
+    travadoTela = !travadoTela;
+
+    if (travadoTela) {
+      Serial.println("BLOQUEAR_TELA");
+    } else {
+      Serial.println("DESBLOQUEAR_TELA");
+    }
+
+    lastPress2 = agora;
   }
 }
